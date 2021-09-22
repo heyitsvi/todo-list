69cfd2a78b5cd67e8920523604dc671c493c3511
Vivek <vivek99sanjeetsingh@gmail.com>
2021-09-22 13:00:30 +0530

    added font dependency

--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,13 @@ module.exports = {
   watchOptions: {
     poll: true,
     ignored: /node_modules/
-  }
+  },
+  module: {
+    rules: [
+     {
+       test: /\.(woff|woff2|eot|ttf|otf)$/i,
+       type: 'asset/resource',
+     },
+    ],
+  },
 };
